fcfbbc8dd946eb3b5d4f276d284f02d179d042f7
sitalbi <simon0111@live.fr>
2023-04-05 01:22:30 +0900

    update and fixes

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,26 +5,22 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
 
-    private float length, startPosition, distance, temp;
-    [SerializeField] private GameObject camera;
-    [SerializeField] private float parallaxEffect;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    [SerializeField] private Vector2 parallaxEffect;
 
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        temp = (camera.transform.position.x * (1-parallaxEffect));
-        distance = (camera.transform.position.x * parallaxEffect);
-
-        transform.position = new Vector3(startPosition - distance, transform.position.y, transform.position.z);
-
-        /*if (temp > startPosition + length) startPosition += length;
-        else if (temp < startPosition - length) startPosition -= length;*/
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position -= new Vector3(deltaMovement.x * parallaxEffect.x, deltaMovement.y * parallaxEffect.y, 0);
+        lastCameraPosition = cameraTransform.position;
     }
 }
